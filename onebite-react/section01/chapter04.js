d2ff44d7c18e497dd4425cf9e57707a77f9752c5
seongmin36 <whtjdals3625@gmail.com>
2025-08-24 21:32:32 +0900

    fix: 화살표 폴더 오류 수정

---
+++ onebite-react/section01/chapter04.js
@@ -0,0 +1,27 @@
+console.log("chapter04!");
+
+// 1. 변수 초기화
+let age = 27;
+console.log(age);
+age = 30;
+console.log(age);
+
+// 2. 상수
+const birth = "2002.06.14";
+console.log(birth);
+
+// 3. 변수 naming 규칙
+// 3-1 $, _ 제외한 기호 X
+let $_namel;
+
+// 3-2 숫자로 시작 X
+let name1;
+let _2name;
+
+// 3-3 예약어 사용 X
+// let let, let if
+
+// 4. 변수 명령 가이드
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
