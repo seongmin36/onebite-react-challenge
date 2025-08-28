1fb16185c4d6ed70dcc5e2efce5bd385f4908af2
seongmin36 <whtjdals3625@gmail.com>
2025-08-28 16:13:54 +0900

    비동기 작업 처리하기 1.콜백함수 finished

---
+++ onebite-react/section02/chapter12.js
@@ -0,0 +1,46 @@
+// 음식을 주문하는 상황
+
+function task() {
+  setTimeout(() => {
+    console.log("안녕하세요");
+  }, 3000);
+}
+
+task();
+
+function orderFood(callback) {
+  setTimeout(() => {
+    const food = "떡볶이";
+    callback(food);
+  }, 3000);
+}
+
+function cooldownFood(food, callback) {
+  setTimeout(() => {
+    const cooldownedFood = `식은 ${food}`;
+    callback(cooldownedFood);
+  });
+}
+
+cooldownFood("라면", (food) => {
+  console.log(`${food}은/는 식었습니다.`);
+});
+
+function freezeFood(food, callback) {
+  setTimeout(() => {
+    const freezedFood = `냉동된 ${food}`;
+    callback(freezedFood);
+  }, 1500);
+}
+
+orderFood((food) => {
+  console.log(food);
+
+  cooldownFood(food, (cooldownedFood) => {
+    console.log(cooldownedFood);
+
+    freezeFood(cooldownedFood, (freezedFood) => {
+      console.log(freezedFood);
+    });
+  });
+});
