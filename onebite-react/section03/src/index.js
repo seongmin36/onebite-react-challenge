b39a37951b124d0f38210c9b7adbeb16b116aab4
seongmin36 <whtjdals3625@gmail.com>
2025-08-29 11:39:21 +0900

    Node 모듈 라이브러리 사용하기 finished

--- onebite-react/section03/src/index.js
+++ onebite-react/section03/src/index.js
@@ -1,6 +1,11 @@
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(2, 1));
-console.log(mul(1, 3));
-console.log("안녕 Node.js");
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(2, 1));
+// console.log(mul(1, 3));
+// console.log("안녕 Node.js");
