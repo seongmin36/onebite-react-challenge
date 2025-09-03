c0e0645328560a479b57d326f1ddf6c07655c0b4
krong <whtjdals3625@gmail.com>
2025-09-03 16:29:32 +0900

    section05 이벤트 처리하기 finished

--- onebite-react/section05/src/components/Button.jsx
+++ onebite-react/section05/src/components/Button.jsx
@@ -1,6 +1,16 @@
-const Button = ({ children, text, color = "black" }) => {
+const Button = ({ children, text, color = 'black' }) => {
+  // 이벤트 객체 e : cross browising issue에서 자유롭다
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text}
       {children}
     </button>
