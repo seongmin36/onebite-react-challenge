8657f78584699d773ef9638271b4a92f9a99f3f9
seongmin36 <whtjdals3625@gmail.com>
2025-09-05 22:44:58 +0900

    section05 useRef로 컴포넌트의 변수 생성하기 finished

--- onebite-react/section05/src/components/Regiter.jsx
+++ onebite-react/section05/src/components/Regiter.jsx
@@ -1,10 +1,11 @@
+import { useRef } from "react";
 import { useState } from "react";
-
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
 // 3, 국적
 // 4. 자기소개
+
 const Register = () => {
   const [input, setInput] = useState({
     name: "",
@@ -12,19 +13,31 @@ const Register = () => {
     country: "",
     bio: "",
   });
+  // useState와 달리 값이 변경돼도 리렌더링 되지 않음. (참조의 역할)
+  const countRef = useRef(0);
+  const inputRef = useRef();
 
   const onChange = (e) => {
-    console.log(e.target.name, e.target.value);
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value, // [] : 프로퍼티의 키, ":" 인수
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -51,6 +64,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
