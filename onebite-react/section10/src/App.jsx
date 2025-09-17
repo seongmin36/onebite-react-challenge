3497080b0686cdb505df4cf8ca15cc5843937d63
krong <whtjdals3625@gmail.com>
2025-09-17 16:41:53 +0900

    section10 useMemo와 연산 최적화 finished

---
+++ onebite-react/section10/src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css';
+import Editer from './components/Editor';
+import Header from './components/Header';
+import List from './components/List';
+import { useRef, useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((todo) =>
+        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
+      );
+    case 'DELETE':
+      return state.filter((todo) => todo.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: Date.now(),
+      },
+    });
+  };
+
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에
+    // targetid와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId,
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editer onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default App;
---
+++ onebite-react/section10/src/components/List.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import './List.css';
+import TodoItem from './TodoItem';
+import { useMemo } from 'react';
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState('');
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      // 대소문자 구별 X
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  // 리렌더링 될때마다 호출
+  const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log('getAnalyzedData 호출');
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length; // filter() 메서드는 배열 길이가 길어질수록 순회하여 계산하기 때문에 굉장히 오래걸림
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]); // todos가 바뀔때만 실행
+
+  // 의존성 배열: deps -> 의존성 배열이 변경될때만 useMemo()가 동작 useEffect()와 동일 (라이프사이클 mount, unmount)
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChange}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => (
+          // 스프레드 연산자로 todo 배열 전달
+          <TodoItem
+            key={todo.id}
+            {...todo}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default List;
