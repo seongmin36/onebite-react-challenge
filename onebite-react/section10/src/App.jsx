3fee27eecb7448d3bc147c244087745ec27f4dbc
seongmin36 <whtjdals3625@gmail.com>
2025-09-18 01:35:00 +0900

    section10 useCallback()과 함수 재생성 방지

--- onebite-react/section10/src/App.jsx
+++ onebite-react/section10/src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css';
-import Editer from './components/Editor';
-import Header from './components/Header';
-import List from './components/List';
-import { useRef, useReducer } from 'react';
+import "./App.css";
+import Editer from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+import { useRef, useReducer, useCallback } from "react";
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CREATE':
+    case "CREATE":
       return [action.data, ...state];
-    case 'UPDATE':
+    case "UPDATE":
       return state.map((todo) =>
         todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
       );
-    case 'DELETE':
+    case "DELETE":
       return state.filter((todo) => todo.id !== action.targetId);
     default:
       return state;
@@ -23,9 +23,9 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
-      type: 'CREATE',
+      type: "CREATE",
       data: {
         id: idRef.current++,
         isDone: false,
@@ -33,25 +33,27 @@ function App() {
         date: Date.now(),
       },
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     // todos State의 값들 중에
     // targetid와 일치하는 id를 갖는 투두 아이템의 isDone 변경
 
     // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
     dispatch({
-      type: 'UPDATE',
+      type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
-      type: 'DELETE',
+      type: "DELETE",
       targetId: targetId,
     });
-  };
+  }, []);
+
+  useCallback(() => {}, []);
 
   return (
     <div className="App">
--- onebite-react/section10/src/components/TodoItem.jsx
+++ onebite-react/section10/src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
-import './TodoItem.css';
-import { memo } from 'react';
+import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ content, date, id, isDone, onUpdate, onDelete }) => {
   return (
@@ -12,17 +12,15 @@ const TodoItem = ({ content, date, id, isDone, onUpdate, onDelete }) => {
   );
 };
 
+// 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
 export default memo(TodoItem, (prevProps, nextProps) => {
-  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
   // T -> Props 바뀌지  않음 -> 리렌더링 X
   // F -> Props 바뀜 -> 리렌더링 O
-
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.date !== nextProps.date) return false;
-
-  return true;
+  // if (prevProps.id !== nextProps.id) return false;
+  // if (prevProps.content !== nextProps.content) return false;
+  // if (prevProps.isDone !== nextProps.isDone) return false;
+  // if (prevProps.date !== nextProps.date) return false;
+  // return true;
 });
 // memo 메서드는 얕은비교(===)를 하기 때문에 call by value(Object 타입)로 호출된 다른 메서드(onUpdate, onDelete)가
 // 주소값이 달라 다른 값으로 판단하여 매번 리렌더링을 시도함.
