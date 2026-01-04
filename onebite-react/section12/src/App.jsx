aa7d23c858ef201824f4b80002cc149c337e129e
seongmin36 <whtjdals3625@gmail.com>
2026-01-04 22:33:24 +0900

    section12 : react-router-dom

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -1,10 +1,39 @@
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import NotFound from "./pages/NotFound";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기장을 상세히 조회하는 Diary 페이지
 function App() {
-  return <></>;
+  const nav = useNavigate();
+
+  const onClickButton = (path) => {
+    nav(path);
+  };
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/diary">Diary</Link>
+      </div>
+      <button onClick={() => onClickButton("/")}>Home 페이지로 이동</button>
+      <button onClick={() => onClickButton("/new")}>New 페이지로 이동</button>
+      <button onClick={() => onClickButton("/diary")}>
+        Diary 페이지로 이동
+      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
