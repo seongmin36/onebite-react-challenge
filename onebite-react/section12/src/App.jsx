d2cfff75108a101c1676ab1053b603906a63e1ad
seongmin36 <whtjdals3625@gmail.com>
2026-01-06 01:21:16 +0900

    section12: 일기 관리 기능 구현하기 1

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -1,16 +1,52 @@
 import { Routes, Route } from "react-router-dom";
+import { useReducer } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 import Button from "./components/Button";
 import Header from "./components/Header";
 
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date().getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
+  {
+    id: 4,
+    createdDate: new Date().getTime(),
+    emotionId: 4,
+    content: "4번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기장을 상세히 조회하는 Diary 페이지
+// 3. "/diary/:id" : 일기장을 상세히 조회하는 Diary 페이지
+// 4. "/edit/:id" : 일기장을 수정하는 Edit 페이지
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
   return (
     <>
       <Header
@@ -38,6 +74,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
---
+++ onebite-react/section12/src/pages/Edit.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const Edit = () => {
+  const params = useParams();
+
+  return <div>Edit {params.id}번 일기 수정 페이지 입니다.</div>;
+};
+
+export default Edit;
