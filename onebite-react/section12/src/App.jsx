8323f5ad4b2d95592a8e76d920b8568f8ec2be21
seongmin36 <whtjdals3625@gmail.com>
2026-01-06 00:07:30 +0900

    section12: 폰트, 이미지, 레이아웃 설정하기

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
+import getEmotionImage from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -16,6 +17,16 @@ function App() {
   };
   return (
     <>
+      {/* public 폴더와 assets 폴더 차이점 :
+      public 폴더는 빌드 시 정적 파일로 복사되지 않는다.
+      assets 폴더는 빌드 시 정적 파일로 복사된다. */}
+      <div>
+        <img src={getEmotionImage(1)} alt="emotion1" />
+        <img src={getEmotionImage(2)} alt="emotion2" />
+        <img src={getEmotionImage(3)} alt="emotion3" />
+        <img src={getEmotionImage(4)} alt="emotion4" />
+        <img src={getEmotionImage(5)} alt="emotion5" />
+      </div>
       <div>
         <Link to="/">Home</Link>
         <Link to="/new">New</Link>
